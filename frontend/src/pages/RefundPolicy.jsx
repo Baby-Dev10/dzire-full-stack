1c385eb81725639ccb4fc5e90e6476471c727753
Baby-Dev10 <exclusivep009@gmail.com>
2025-01-11 11:49:14 +0530

    update

--- frontend/src/pages/RefundPolicy.jsx
+++ frontend/src/pages/RefundPolicy.jsx
@@ -6,7 +6,7 @@ const RefundPolicy = () => {
   }, []);
   return (
     <div>
-      <h1 className="flex justify-center text-2xl text-black font-extrabold">
+      <h1 className="flex justify-center text-2xl text-black font-bold">
         Refund Policy
       </h1>
       <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0">
@@ -20,21 +20,21 @@ const RefundPolicy = () => {
         materials, or flammable liquids or gases.
       </p>
       <div>
-        <p className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto text-bold">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 text-bold">
           Additional non-returnable items:
         </p>
         <ul>
-          <li className="text-justify flex text-s w-1/2 px-2 py-1 mx-auto ">
+          <li className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 ">
             1. Gift cards.
           </li>
-          <li className="text-justify flex text-s w-1/2 px-2 py-1 mx-auto">
+          <li className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
             2. Downloadable software products.
           </li>
-          <li className="text-justify flex text-s w-1/2 px-2 py-1 mx-auto">
+          <li className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
             3. Some health and personal care items.
           </li>
         </ul>
-        <p className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           To complete your return, we require a receipt or proof of purchase.
           Please do not send your purchase back to the manufacturer. There are
           certain situations where only partial refunds are granted: (if
@@ -44,10 +44,10 @@ const RefundPolicy = () => {
           parts for reasons not due to our error. Any item that is returned more
           than 30 days after delivery
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           Refunds (if applicable)
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           Once your return is received and inspected, we will send you an email
           to notify you that we have received your returned item. We will also
           notify you of the approval or rejection of your refund. If you are
@@ -55,10 +55,10 @@ const RefundPolicy = () => {
           automatically be applied to your credit card or original method of
           payment, within a certain amount of days.
         </p>
-        <ul className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto font-medium">
+        <ul className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           Late or missing refunds (if applicable)
         </ul>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           If you haven't received a refund yet, first check your bank account
           again. Then contact your credit card company, it may take some time
           before your refund is officially posted. Next contact your bank. There
@@ -66,40 +66,40 @@ const RefundPolicy = () => {
           done all of this and you still have not received your refund yet,
           please contact us at [___Company Contact Email___].
         </p>
-        <ul className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto font-medium">
+        <ul className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           Sale items (if applicable)
         </ul>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           Only regular priced items may be refunded, unfortunately sale items
           cannot be refunded.
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto font-medium">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           Exchanges (if applicable)
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           We only replace items if they are defective or damaged. If you need to
           exchange it for the same item, send us an email at [___Company Contact
           Email___] and send your item to: 622 Manglam Electronic Market Jaipur
           Rajasthan India 302001.
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 py-4 mx-auto font-medium">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-medium">
           Gifts
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           If the item was marked as a gift when purchased and shipped directly
           to you, you'll receive a gift credit for the value of your return.
           Once the returned item is received, a gift certificate will be mailed
           to you.
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 py-3 mx-auto ">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           If the item wasn't marked as a gift when purchased, or the gift giver
           had the order shipped to themselves to give to you later, we will send
           a refund to the gift giver and he will find out about your return.
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 py-3 mx-auto font-semibold">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0 font-semibold">
           Shipping
         </p>
-        <p className="text-justify flex text-s w-1/2 px-2 mx-auto">
+        <p className="text-justify flex text-s md:w-1/2 md:px-2 py-4 mx-auto w-full px-0">
           To return your product, you should mail your product to: 622 Manglam
           Electronic Market Jaipur Rajasthan India 302001. You will be
           responsible for paying for your own shipping costs for returning your
--- frontend/src/pages/ShippingPolicy.jsx
+++ frontend/src/pages/ShippingPolicy.jsx
@@ -6,9 +6,41 @@ const ShippingPolicy = () => {
   }, []);
   return (
     <div>
-      <h1 className="flex justify-center text-2xl text-black font font-extrabold">
+      <h1 className="flex justify-center text-2xl text-black font font-bold">
         Shipping Policy
       </h1>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0 font-medium">
+        It's important to start by clarifying to customers that your order
+        processing times are separate from the shipping times they see at
+        checkout.{" "}
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0">
+        All orders are processed within X to X business days (excluding weekends
+        and holidays) after receiving your order confirmation email. You will
+        receive another notification when your order has shipped.{" "}
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0 font-medium">
+        Include any other pertinent information towards the beginning, such as
+        potential delays due to a high volume of orders or postal service
+        problems that are outside of your control.
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0">
+        Domestic Shipping Rates and Estimates
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0">
+        For calculated shipping rates: Shipping charges for your order will be
+        calculated and displayed at checkout.{" "}
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0">
+        Deliveries are made from [delivery hours] on [available days]. We will
+        contact you via text message with the phone number you provided at
+        checkout to notify you on the day of our arrival.{" "}
+      </p>
+      <p className="text-justify flex text-s md:w-1/2 md:px-2  py-4 mx-auto w-full px-0 font-medium">
+        You can list out the ZIP/postal codes you service and/or consider
+        embedding a map here so customers can easily see if they are within your
+        local delivery range.
+      </p>
     </div>
   );
 };
